39949bb6fe7d7c6e0874ffd2d24cf7eee513ee64
Uwe Kleinmann <uwe@kleinmann.org>
2019-03-26 00:43:22 +0100

    Add keymap from https://configure.ergodox-ez.com/layouts/n7d0/latest/0 along with space cadet changes for german layouts.

--- keyboards/ergodox_ez/keymaps/kleinmann/config.h
+++ keyboards/ergodox_ez/keymaps/kleinmann/config.h
@@ -2,3 +2,12 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+#undef LSPO_KEY
+#define LSPO_KEY KC_8
+#undef RSPC_KEY
+#define RSPC_KEY KC_9
+#undef DISABLE_SPACE_CADET_ROLLOVER
+#define DISABLE_SPACE_CADET_ROLLOVER
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 130
